b881f7e529cfed02a9a9dac040e3d342f7e91245
tatangjuliyanto <tatangjulianto05@gmail.com>
2025-05-30 21:26:30 +0700

    fix the code

--- lib/presentation/pages/product_page.dart
+++ lib/presentation/pages/product_page.dart
@@ -10,27 +10,30 @@ class ProductPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: BlocBuilder<ProductBloc, ProductState>(
-        builder: (context, state) {
-          if (state is ProductLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is ProductLoaded) {
-            return ListView.builder(
-              itemCount: state.products.length,
-              itemBuilder: (context, index) {
-                final product = state.products[index];
-                return ListTile(
-                  leading: Image.network(product.thumbnail),
-                  title: Text(product.title),
-                  subtitle: Text('\$${product.price.toString()}'),
-                );
-              },
-            );
-          } else if (state is ProductError) {
-            return Center(child: Text(state.message));
-          }
-          return const Center(child: Text('No Products'));
-        },
+      body: BlocProvider(
+        create: (context) => ProductBloc(getProducts: context.read()),
+        child: BlocBuilder<ProductBloc, ProductState>(
+          builder: (context, state) {
+            if (state is ProductLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is ProductLoaded) {
+              return ListView.builder(
+                itemCount: state.products.length,
+                itemBuilder: (context, index) {
+                  final product = state.products[index];
+                  return ListTile(
+                    leading: Image.network(product.thumbnail),
+                    title: Text(product.title),
+                    subtitle: Text('\$${product.price.toString()}'),
+                  );
+                },
+              );
+            } else if (state is ProductError) {
+              return Center(child: Text(state.message));
+            }
+            return const Center(child: Text('No Products'));
+          },
+        ),
       ),
     );
   }
