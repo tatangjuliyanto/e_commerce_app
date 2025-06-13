0966b684e9a27e45bd9261b19e1a61a43413faeb
tatangjuliyanto <tatangjulianto05@gmail.com>
2025-06-13 16:39:56 +0700

    fix the code

--- lib/features/product/data/repositories/product_repository_impl.dart
+++ lib/features/product/data/repositories/product_repository_impl.dart
@@ -1,4 +1,3 @@
-import 'package:e_commerce_app/features/product/data/models/product_model.dart';
 import 'package:e_commerce_app/features/product/data/sources/product_remote_data_source.dart';
 import '../../domain/entities/product.dart';
 import '../../domain/repositories/product_repository.dart';
