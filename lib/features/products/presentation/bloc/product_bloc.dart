c244975111d97f714b0d8364dc13b6e071a3ff30
tatangjuliyanto <tatangjulianto05@gmail.com>
2025-09-26 21:12:29 +0700

    fix the code

--- lib/features/products/presentation/bloc/product_bloc.dart
+++ lib/features/products/presentation/bloc/product_bloc.dart
@@ -12,7 +12,6 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
     : super(ProductInitial()) {
     on<LoadProducts>(_onLoadProducts);
     on<LoadProductDetail>(_onLoadProductDetail);
-    on<AddToCartEvent>(_onAddToCart);
   }
 
   Future<void> _onLoadProducts(
@@ -36,24 +35,6 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
     try {
       final product = await getProductDetail(event.productId);
       emit(ProductDetailLoaded(product: product));
-    } catch (e) {
-      emit(ProductError(message: e.toString()));
-    }
-  }
-
-  Future<void> _onAddToCart(
-    AddToCartEvent event,
-    Emitter<ProductState> emit,
-  ) async {
-    try {
-      if (state is ProductDetailLoaded) {
-        final currentState = state as ProductDetailLoaded;
-        // Assuming you have a cart model and a way to add products to it
-        // For demonstration, just emit a state indicating added to cart
-        // emit(ProductAddedToCart(product: currentState.product));
-      } else {
-        emit(ProductError(message: "Product details not loaded."));
-      }
     } catch (e) {
       emit(ProductError(message: e.toString()));
     }
